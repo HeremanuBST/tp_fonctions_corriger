1e846d5c8c17a8f0832c3e690c2a0fb4d98b8888
Heremanu <heremanu.besset@edu.ece.fr>
2025-10-24 14:33:50 +0200

    feat : Add rectangle and triangle

---
+++ Rectangle.h
@@ -0,0 +1,27 @@
+//
+// Created by Heremanu on 24/10/2025.
+//
+
+#ifndef TP_FONCTIONS_RECTANGLE_H
+#define TP_FONCTIONS_RECTANGLE_H
+
+#define RECTANGLE 1
+
+/**
+ * Cette fonction affiche un rectangle
+ * @param hauteur C'est la hauteur du rectangle
+ * @param largeur C'est la largeur du rectangle
+ */
+void afficherRectangle(int hauteur, int largeur);
+
+/**
+ * Cette fonction affiche l'air d'un rectangle
+ * @param hauteur C'est la hauteur du rectangle
+ * @param largeur C'est la largeur du rectangle
+ * @return
+ */
+int aireRectangle(int hauteur, int largeur);
+void initRectangle(int* phauteur, int* plargeur);
+void Rectangle(void);
+
+#endif //TP_FONCTIONS_RECTANGLE_H
